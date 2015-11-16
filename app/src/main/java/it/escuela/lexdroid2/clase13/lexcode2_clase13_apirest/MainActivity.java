68b20f672fe8a2259e5fd34ac5d1c38a23bcb5b1
marcos sanchez <marcos.visualnt@gmail.com>
2015-11-16 22:52:00 +0100

    Initial commit

---
+++ app/src/main/java/it/escuela/lexdroid2/clase13/lexcode2_clase13_apirest/MainActivity.java
@@ -0,0 +1,34 @@
+package it.escuela.lexdroid2.clase13.lexcode2_clase13_apirest;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private class SyncService extends AsyncTask<String, String, Boolean> {
+        JSONObject responseJSON;
+        String urlCompose;
+        private ProgressDialog dialog;
+
+        public void validateSyc(Context context){
+            dialog = new ProgressDialog(context);
+
+        }
+
+        
+
+
+    }
+
+}
