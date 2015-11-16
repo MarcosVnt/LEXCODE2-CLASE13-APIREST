848a04cd3f1021face913c22ca8b5e0938fecaad
marcos sanchez <marcos.visualnt@gmail.com>
2015-11-16 23:09:52 +0100

    3.- completamos metodo doInBackground

--- app/src/main/java/it/escuela/lexdroid2/clase13/lexcode2_clase13_apirest/MainActivity.java
+++ app/src/main/java/it/escuela/lexdroid2/clase13/lexcode2_clase13_apirest/MainActivity.java
@@ -5,8 +5,18 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.IOException;
+
+import cz.msebera.android.httpclient.HttpResponse;
+import cz.msebera.android.httpclient.client.HttpClient;
+import cz.msebera.android.httpclient.client.methods.HttpGet;
+import cz.msebera.android.httpclient.impl.client.DefaultHttpClient;
+import cz.msebera.android.httpclient.util.EntityUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,10 +38,39 @@ public class MainActivity extends AppCompatActivity {
 
         protected Boolean doInBackground(String... params){
             boolean result = true;
+            HttpClient httpClient = new DefaultHttpClient();
+            urlCompose="http://gs500.mx/AppGeolocalizacion/GetStations.php?token=3b120d6dd087042cc46bfc0b94b07382";
+            HttpGet get= new HttpGet(urlCompose);
+            get.setHeader("content-type", "application/json");
+
+
+
+            try {
+                Log.d("Conectaados a : ", urlCompose);
+                HttpResponse response = httpClient.execute(get);
+                String respStr= EntityUtils.toString(response.getEntity());
+                JSONObject respJSON = new JSONObject(respStr);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e){
+                e.printStackTrace();
+                //varias formas de manejar excepciones , subirla a la clase de arriba o manejarla aqui como ahora
+            }
+
 
             return result;
         }
 
+        protected void onPreExcute(){
+            this.dialog.setMessage("Sincronizando .. espere");
+            this.dialog.show();
+
+        }
+        protected void onPostExecute(){
+            //recibir respuesta
+
+        }
 
     }
 
